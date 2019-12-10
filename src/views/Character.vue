b1528c54b376bdbdc5687315ffbca87665520214
Opion <kindledkindred@gmail.com>
2019-12-10 22:10:46 +0900

    レイアウトを変更，サブ装備と指輪の表示を追加

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -13,34 +13,35 @@
           .level {{ level }}
       //- status
       v-row(justify="center")
-        v-col(cols="4")
+        v-col.py-1(cols="3")
           v-text-field(v-model.number="vit" type="number" label="体力")
+        v-col.py-1(cols="3")
           v-text-field(v-model.number="str" type="number" label="筋力")
+        v-col.py-1(cols="3")
           v-text-field(v-model.number="pow" type="number" label="魔力")
+        v-col.py-1(cols="3")
           v-text-field(v-model.number="dex" type="number" label="技量")
-        v-col(cols="8")
-          v-row(justify="center")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="HP" readonly outlined dense label="HP")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="MP" readonly outlined dense label="MP")
-          v-row(justify="center")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="physicalAbility" readonly outlined dense label="物理")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="magicAbility" readonly outlined dense label="魔法")
-          v-row(justify="center")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="deft" readonly outlined dense label="技巧")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="parryingRate" readonly outlined dense label="受流")
-          v-row(justify="center")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="potency" readonly outlined dense label="威力")
-            v-col.py-1(cols="6")
-              v-text-field(v-model="criticalRate" readonly outlined dense label="会心")
+      v-row(justify="center")
+        v-col.py-1(cols="3")
+          v-text-field(v-model="physicalAbility" readonly outlined dense label="物理")
+        v-col.py-1(cols="3")
+          v-text-field(v-model="magicAbility" readonly outlined dense label="魔法")
+        v-col.py-1(cols="3")
+          v-text-field(v-model="deft" readonly outlined dense label="技巧")
+        v-col.py-1(cols="3")
+          v-text-field(v-model="parryingRate" readonly outlined dense label="受流")
+      v-row(justify="center")
+        v-col.pt-0(cols="3")
+          v-text-field(v-model="mainPotency" readonly outlined dense label="威力 - メイン")
+        v-col.pt-0(cols="3")
+          v-text-field(v-model="mainCriticalRate" readonly outlined dense label="会心 - メイン")
+        v-col.pt-0(cols="3")
+          v-text-field(v-model="subPotency" readonly outlined dense label="威力 - サブ")
+        v-col.pt-0(cols="3")
+          v-text-field(v-model="subCriticalRate" readonly outlined dense label="会心 - サブ")
       //- equipment
       v-content
+        //- main weapon
         v-row(justify="center")
           v-col(cols="12" sm="6")
             v-select(
@@ -48,7 +49,7 @@
               :items="weapons"
               item-text="name"
               item-value="id"
-              label="武器"
+              label="メイン武器"
               :hint="`威力${weapons[currentWeaponId].basePotency}, 物理${weapons[currentWeaponId].physicalRatio * 100}, 魔法${weapons[currentWeaponId].magicRatio * 100}, 技巧${weapons[currentWeaponId].deftRatio * 100}`"
               persistent-hint
             )
@@ -58,16 +59,54 @@
               :items="weaponStones"
               item-text="name"
               item-value="id"
-              label="付呪"
+              label="変質石"
               :hint="`${weaponStones[currentWeaponStoneId].description}`"
               persistent-hint
             )
+        //- sub weapon
         v-row(justify="center")
-          v-col(cols="6")
-          v-col(cols="6")
+          v-col(cols="12" sm="6")
+            v-select(
+              v-model="currentSubWeaponId"
+              :items="weapons"
+              item-text="name"
+              item-value="id"
+              label="サブ武器"
+              :hint="`威力${weapons[currentSubWeaponId].basePotency}, 物理${weapons[currentSubWeaponId].physicalRatio * 100}, 魔法${weapons[currentSubWeaponId].magicRatio * 100}, 技巧${weapons[currentSubWeaponId].deftRatio * 100}`"
+              persistent-hint
+            )
+          v-col(cols="12" sm="6")
+            v-select(
+              v-model="currentSubWeaponStoneId"
+              :items="weaponStones"
+              item-text="name"
+              item-value="id"
+              label="変質石"
+              :hint="`${weaponStones[currentSubWeaponStoneId].description}`"
+              persistent-hint
+            )
+        //- ring
         v-row(justify="center")
-          v-col(cols="6")
-          v-col(cols="6")
+          v-col(cols="12" sm="6")
+            v-select(
+              v-model="currentRingId"
+              :items="rings"
+              item-text="name"
+              item-value="id"
+              label="指輪"
+              :hint="`${rings[currentRingId].description}`"
+              persistent-hint
+            )
+          v-col(cols="12" sm="6")
+            v-select(
+              v-model="currentRingSignId"
+              :items="ringSigns"
+              item-text="name"
+              item-value="id"
+              label="刻印"
+              :hint="`${ringSigns[currentRingSignId].description}`"
+              persistent-hint
+            )
       //- battle skill
       v-row(justify="center")
       //- field skill
@@ -98,6 +137,8 @@ export default class Character extends Vue {
   str: number = 5;
   pow: number = 5;
   dex: number = 5;
+  currentHP: number = this.HP;
+  currentMP: number = this.MP;
 
   /**
    * Equipment
@@ -108,7 +149,6 @@ export default class Character extends Vue {
    *   スキル威力は攻撃力を元に計算され，スキル自体が物理／魔法の属性を持つ．
    *   これにより「物理と魔法に補正され，物理スキルを放つ武器」が実装可能．
    */
-  currentWeaponId: number = 0;
   weapons = [
     {
       id: "",
@@ -146,9 +186,8 @@ export default class Character extends Vue {
     }
   ];
   /**
-   * メイン武器の変質石
+   * 2. weaponStone
    */
-  currentWeaponStoneId: number = 0;
   weaponStones = [
     {
       id: 0,
@@ -168,67 +207,8 @@ export default class Character extends Vue {
     }
   ];
   /**
-   * サブ武器
+   * 3. 指輪
    */
-  currentSubWeaponId: number = 0;
-  subWeapons = [
-    {
-      id: "",
-      name: "",
-      description: "",
-      basePotency: 0,
-      physicalRatio: 0, // 物理攻撃の，威力への反映率
-      magicRatio: 0, // 魔法攻撃の，威力への反映率
-      deftRatio: 0, // 技巧の，クリティカル率に対する 反映率
-      skill1: {
-        name: "",
-        description: "",
-        isPassive: false,
-        element: "",
-        potencyRatio: 0,
-        range: 0,
-        mpCost: 0,
-        target: ""
-      },
-      skill2: {
-        name: "",
-        description: "",
-        isPassive: false,
-        element: "",
-        potencyRatio: 0,
-        range: 0,
-        mpCost: 0,
-        target: ""
-      }
-    }
-  ];
-  /**
-   * サブ武器の変質石
-   */
-  currentSubWeaponStoneId: number = 0;
-  subWeaponStones = [
-    {
-      id: 0,
-      name: "",
-      description: "",
-      potencyRatio: 1,
-      physicalRatio: 1,
-      magicRatio: 1,
-      deftRatio: 1,
-      physicalRate: 0,
-      magicRate: 0,
-      deftRate: 0,
-      skill1Ratio: 0,
-      skill2Ratio: 0,
-      skill3Ratio: 0,
-      skill1ChangedTarget: "",
-      skill2ChangedTarget: ""
-    }
-  ];
-  /**
-   * 指輪
-   */
-  currentRingId: number = 0;
   rings = [
     {
       id: 0,
@@ -247,25 +227,31 @@ export default class Character extends Vue {
     }
   ];
   /**
-   * 指輪の刻印
+   * 4. 指輪の刻印
    */
-  currentRingSignId: number = 0;
   ringSigns = [
     {
       id: 0,
       name: "",
-      description: "",
-      ultRatio: 1,
-      ultCriticalRate: 0,
-      ultChangedTargetArea: ""
+      description: ""
     }
   ];
+
+  /**
+   * 現在の装備を示す id
+   */
+  currentWeaponId: number = 0;
+  currentWeaponStoneId: number = 0;
+  currentSubWeaponId: number = 0;
+  currentSubWeaponStoneId: number = 0;
+  currentRingId: number = 0;
+  currentRingSignId: number = 0;
+
 
   get level(): number {
     const level = this.vit + this.str + this.dex + this.pow;
     return level;
   }
-
   get HP(): number {
     let tmpHP = this.growthCurve(this.vit, 50, 30, 18, 10, 6, 3);
     tmpHP = this.growthCurve(this.str, 4.5, 1.8, 0.9, 0.9, 0.9, 1, tmpHP);
@@ -286,35 +272,20 @@ export default class Character extends Vue {
   get parryingRate(): number {
     return this.growthCurve(this.dex, 1.2, 0.4, 0.1, 0.1, 0.1, 0.112);
   }
-  get potency(): number {
-    return Math.floor(
-      /** 基礎攻撃力 */
-      (this.weapons[this.currentWeaponId].basePotency +
-        /** 物理補正 */
-        (this.weapons[this.currentWeaponId].physicalRatio *
-          this.weaponStones[this.currentWeaponStoneId].physicalRatio +
-          this.weaponStones[this.currentWeaponStoneId].physicalRate) *
-          this.physicalAbility +
-        /** 魔法補正 */
-        (this.weapons[this.currentWeaponId].magicRatio *
-          this.weaponStones[this.currentWeaponStoneId].magicRatio +
-          this.weaponStones[this.currentWeaponStoneId].magicRate) *
-          this.magicAbility) *
-        /** 威力補正 */
-        this.weaponStones[this.currentWeaponStoneId].potencyRatio
-    );
+  get mainPotency(): number {
+    return this.calculatePotency(this.currentWeaponId, this.currentWeaponStoneId)
   }
-  get criticalRate(): number {
-    return Math.floor(
-      /** 技量補正 */
-      (this.weapons[this.currentWeaponId].deftRatio *
-        this.weaponStones[this.currentWeaponStoneId].deftRatio +
-        this.weaponStones[this.currentWeaponStoneId].deftRate) *
-        this.deft *
-        0.24
-    );
+  get mainCriticalRate(): number {
+    return this.calculateCriticalRate(this.currentWeaponId, this.currentWeaponStoneId)
+  }
+  get subPotency(): number {
+    return this.calculatePotency(this.currentSubWeaponId, this.currentSubWeaponStoneId)
+  }
+  get subCriticalRate(): number {
+    return this.calculateCriticalRate(this.currentSubWeaponId, this.currentSubWeaponStoneId)
   }
 
+  /** レベル帯に成長する値を入力すれば最終的なステータスが返されます */
   growthCurve(
     mainStatus: number,
     growthUnder20: number,
@@ -343,28 +314,77 @@ export default class Character extends Vue {
     }
     return Math.floor(subStatus);
   }
+  /** 武器idと変質石idから，現在のステータスにおける威力を算出します */
+  calculatePotency(weaponId: number, weaponStoneId: number): number {
+    return Math.floor(
+      /** 基礎攻撃力 */
+      (this.weapons[weaponId].basePotency +
+        /** 物理補正 */
+        (this.weapons[weaponId].physicalRatio *
+          this.weaponStones[weaponStoneId].physicalRatio +
+          this.weaponStones[weaponStoneId].physicalRate) *
+          this.physicalAbility +
+        /** 魔法補正 */
+        (this.weapons[weaponId].magicRatio *
+          this.weaponStones[weaponStoneId].magicRatio +
+          this.weaponStones[weaponStoneId].magicRate) *
+          this.magicAbility) *
+        /** 威力補正 */
+        this.weaponStones[weaponStoneId].potencyRatio
+    );
+  }
+  /** 武器idと変質石idから，現在のステータスにおける会心率を算出します */
+  calculateCriticalRate(weaponId: number, weaponStoneId: number): number {
+    return Math.floor(
+      /** 技量補正 */
+      (this.weapons[weaponId].deftRatio *
+        this.weaponStones[weaponStoneId].deftRatio +
+        this.weaponStones[weaponStoneId].deftRate) *
+        this.deft *
+        0.24
+    );
+  }
 
   mounted() {
     /**
      * Firestore から 装備品データを読み込む
+     * 本当は for 文とかでスマートに取得したかった．
      */
     db.collection("weapons")
       .get()
       .then(querySnapshot => {
-        let tmpWeapons: any = [];
+        let tmp: Array<any> = []
         querySnapshot.forEach(doc => {
-          tmpWeapons.push(doc.data());
+          tmp.push(doc.data());
         });
-        this.weapons = tmpWeapons;
+        this.weapons = tmp
       });
     db.collection("weaponStones")
       .get()
       .then(querySnapshot => {
-        let tmpWeaponStones: any = [];
+        let tmp: Array<any> = []
         querySnapshot.forEach(doc => {
-          tmpWeaponStones.push(doc.data());
+          tmp.push(doc.data());
         });
-        this.weaponStones = tmpWeaponStones;
+        this.weaponStones = tmp
+      });
+    db.collection("rings")
+      .get()
+      .then(querySnapshot => {
+        let tmp: Array<any> = []
+        querySnapshot.forEach(doc => {
+          tmp.push(doc.data());
+        });
+        this.rings = tmp
+      });
+    db.collection("ringSigns")
+      .get()
+      .then(querySnapshot => {
+        let tmp: Array<any> = []
+        querySnapshot.forEach(doc => {
+          tmp.push(doc.data());
+        });
+        this.ringSigns = tmp
       });
   }
 }
