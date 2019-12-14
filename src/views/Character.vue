0c7adfee046875e02c378c3290bd6afc1a457144
Opion <kindledkindred@gmail.com>
2019-12-14 23:04:46 +0900

    firestore から得たデータの欠損値補完

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -128,6 +128,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import db from "@/plugins/firebase";
+import undefined from 'firebase/empty-import';
 
 @Component
 export default class Character extends Vue {
@@ -378,8 +379,41 @@ export default class Character extends Vue {
       .then(querySnapshot => {
         let tmp: Array<any> = [];
         querySnapshot.forEach(doc => {
-          tmp.push(doc.data());
+          tmp.push(doc.data())
         });
+        /**
+         * 未指定の項目にデフォルト値を適用．並びは firestore に準拠
+         * 最低限の構成
+         *  - id
+         *  - skill1 (中身は空でも構いません)
+         *  - skill2 (中身は空でも構いません)
+         */
+        tmp.map((weapon) => {
+          // !weapon.XXX だと値が0のときも含まれてしまいます
+          // === null だと undefined を漏らしてしまいます
+          if (weapon.basePotency == null) weapon.basePotency = 0
+          if (weapon.deftRatio == null) weapon.deftRatio = 0
+          if (weapon.description == null) weapon.description = ''
+          if (weapon.magicRatio == null) weapon.magicRatio = 0
+          if (weapon.name == null) weapon.name = `仮武器${weapon.id}`
+          if (weapon.physicalRatio == null) weapon.physicalRatio = 0
+          if (weapon.skill1.description == null) weapon.skill1.description = ''
+          if (weapon.skill1.element == null) weapon.skill1.element = '無'
+          if (weapon.skill1.isPassive == null) weapon.skill1.isPassive = false
+          if (weapon.skill1.mpCost == null) weapon.skill1.mpCost = 0
+          if (weapon.skill1.name == null) weapon.skill1.name = '仮スキル1'
+          if (weapon.skill1.potencyRatio == null) weapon.skill1.potencyRatio = 0
+          if (weapon.skill1.range == null) weapon.skill1.range = 0
+          if (weapon.skill1.target == null) weapon.skill1.target = '自身'
+          if (weapon.skill2.description == null) weapon.skill2.description = ''
+          if (weapon.skill2.element == null) weapon.skill2.element = '無'
+          if (weapon.skill2.isPassive == null) weapon.skill2.isPassive = false
+          if (weapon.skill2.mpCost == null) weapon.skill2.mpCost = 0
+          if (weapon.skill2.name == null) weapon.skill2.name = '仮スキル2'
+          if (weapon.skill2.potencyRatio == null) weapon.skill2.potencyRatio = 0
+          if (weapon.skill2.range == null) weapon.skill2.range = 0
+          if (weapon.skill2.target == null) weapon.skill2.target = '自身'
+        })
         this.weapons = tmp;
       });
     db.collection("weaponStones")
@@ -389,6 +423,21 @@ export default class Character extends Vue {
         querySnapshot.forEach(doc => {
           tmp.push(doc.data());
         });
+        tmp.map((weaponStone) => {
+          if (weaponStone.deftRate == null) weaponStone.deftRatio = 0
+          if (weaponStone.deftRatio == null) weaponStone.deftRatio = 1
+          if (weaponStone.description == null) weaponStone.description = ''
+          if (weaponStone.magicRate == null) weaponStone.magicRate = 0
+          if (weaponStone.magicRatio == null) weaponStone.magicRatio = 1
+          if (weaponStone.name == null) weaponStone.name == `仮変質石${weaponStone.id}`
+          if (weaponStone.physicalRate == null) weaponStone.physicalRate = 0
+          if (weaponStone.physicalRatio == null) weaponStone.physicalRatio = 1
+          if (weaponStone.potencyRatio == null) weaponStone.potencyRatio = 1
+          if (weaponStone.skill1ChangedTarget == null) weaponStone.skill1ChangedTarget = ''
+          if (weaponStone.skill1Ratio == null) weaponStone.skill1Ratio = 1
+          if (weaponStone.skill2ChangedTarget == null) weaponStone.skill2ChangedTarget = ''
+          if (weaponStone.skill1Ratio == null) weaponStone.skill2Ratio = 1
+        })
         this.weaponStones = tmp;
       });
     db.collection("rings")
@@ -398,6 +447,18 @@ export default class Character extends Vue {
         querySnapshot.forEach(doc => {
           tmp.push(doc.data());
         });
+        tmp.map((ring) => {
+          if (ring.description == null) ring.description = ''
+          if (ring.name == null) ring.name = `仮指輪${ring.id}`
+          if (ring.skill.description == null) ring.skill.description = ''
+          if (ring.skill.element == null) ring.skill.element = '無'
+          if (ring.skill.isPassive == null) ring.skill.isPassive = false
+          if (ring.skill.mpCost == null) ring.skill.mpCost = 4
+          if (ring.skill.name == null) ring.skill.name = `仮スキル`
+          if (ring.skill.potencyRatio == null) ring.skill.potencyRatio = 0
+          if (ring.skill.range == null) ring.skill.range = 0
+          if (ring.skill.target == null) ring.skill.target = '自身'
+        })
         this.rings = tmp;
       });
     db.collection("ringSigns")
@@ -407,6 +468,10 @@ export default class Character extends Vue {
         querySnapshot.forEach(doc => {
           tmp.push(doc.data());
         });
+        tmp.map((ringSign) => {
+          if (ringSign.description == null) ringSign.description = '効果未設定'
+          if (ringSign.name == null) ringSign.name = `仮刻印${ringSign.id}`
+        })
         this.ringSigns = tmp;
       });
   }
